94bc9df10ae182211fedf1f39c49a214ba23dd52
returner <returner@daum.net>
2022-07-04 14:57:43 +0900

    프로젝트, nuget 참조 추가

---
+++ WebSolution/AppEntities/Entities/UserInfo.cs
@@ -0,0 +1,45 @@
+﻿using AppEntities.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEntities.Entities
+{
+    public record UserInfo : IEntityAggrateRoot
+    {
+        public int Id { get; init; }
+        public string? UserKey { get; init; }
+        public string? Name { get; init; }
+        public string? Email { get; init; }
+        public UserProfile? UserProfile { get; set; }
+        public bool IsActivate { get; set; }
+        public DateTime CreateDate { get; init; }
+    }
+
+    public record UserProfile : IEntityAggrateRoot
+    {
+        public int Id { get; init; }
+        public string? FilePath { get; init; }
+        public DateTime CreateDate { get; init; }
+    }
+
+    public record Store
+    {
+        public int Id { get; init; }
+        public string? Name { get; init; }
+        public ICollection<Product>? Products { get; set; }
+        public bool IsUse { get; set; }
+        public DateTime CreateDate { get; init; }
+
+    }
+
+    public record Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ WebSolution/AppEntities/interfaces/IAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEntities.interfaces
+{
+    public interface IAppDbContext : IDisposable
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        DatabaseFacade Database { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
